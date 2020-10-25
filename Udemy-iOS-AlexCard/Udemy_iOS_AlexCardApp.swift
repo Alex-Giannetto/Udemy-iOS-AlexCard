79a395ecface2b760f3ff76492dc3df8afae2c07
Alexandre Giannetto <32279162+Alexg78bis@users.noreply.github.com>
2020-10-25 17:11:55 +0100

    Initial Commit

---
+++ Udemy-iOS-AlexCard/Udemy_iOS_AlexCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Udemy_iOS_AlexCardApp.swift
+//  Udemy-iOS-AlexCard
+//
+//  Created by Alex on 25/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct Udemy_iOS_AlexCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
